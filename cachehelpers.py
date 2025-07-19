61b566b261f6cfaf52027b74835c9b4514487089
japaneseTemmie <japanesetemmie64@proton.me>
2025-07-19 14:21:22 +0200

    Move some helper functions to dedicated files

---
+++ cachehelpers.py
@@ -0,0 +1,15 @@
+from settings import *
+
+# Caching tools
+def store_cache(content: Any, id: str | int, cache: dict) -> None:
+    if content:
+        cache[id] = content
+    else:
+        invalidate_cache(id, cache) # Don't cache empty hashmaps
+
+def get_cache(cache: dict, id: str | int) -> Any | None:
+    if id in cache:
+        return cache.get(id)
+    
+def invalidate_cache(id: str | int, cache: dict) -> None:
+    cache.pop(id, None)
---
+++ timehelpers.py
@@ -0,0 +1,55 @@
+from settings import *
+
+def add_zeroes(parts: list[str], length_limit: int):
+    missing = length_limit - len(parts)
+    
+    for _ in range(missing):
+        parts.insert(0, "00")
+
+def format_seconds(seconds: int) -> str:
+    hours = seconds // 3600
+    minutes = (seconds % 3600) // 60
+    remaining_seconds = seconds % 60
+
+    return f"{hours:02d}:{minutes:02d}:{remaining_seconds:02d}"
+
+def format_minutes(minutes_str: str) -> int | None:
+    try:
+        parts = minutes_str.split(":")
+        
+        if len(parts) < 3:
+            add_zeroes(parts, 3)
+        
+        for i, part in enumerate(parts):
+            if int(part) > 59 and i > 0:
+                return None
+        
+        hours, minutes, seconds = map(int, parts)
+        return int(hours * 3600 + minutes * 60 + seconds)
+    except Exception as e:
+
+        if CAN_LOG and LOGGER is not None and not isinstance(e, ValueError):
+            LOGGER.exception(e)
+
+        return None
+
+def format_minutes_extended(minutes_str: str) -> int | None:
+    try:
+        parts = minutes_str.split(":")
+        
+        if len(parts) < 4:
+            add_zeroes(parts, 4)
+
+        for i, part in enumerate(parts):
+            if (i < 1 and int(part) > 28) or\
+                (i == 1 and int(part) > 23) or\
+                (i > 1 and int(part) > 59):
+                
+                return None
+
+        days, hours, minutes, seconds = map(int, parts)
+        return int(days * 86400 + hours * 3600 + minutes * 60 + seconds)
+    except Exception as e:
+        if CAN_LOG and LOGGER is not None and not isinstance(e, ValueError):
+            LOGGER.exception(e)
+        return None
