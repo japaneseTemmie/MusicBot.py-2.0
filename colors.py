a99d8c543313c85718668308ad01ec131a1649c4
japaneseTemmie <japanesetemmie64@proton.me>
2025-08-14 23:04:15 +0200

    Add more colors

--- colors.py
+++ colors.py
@@ -9,4 +9,30 @@ class Colors:
     CYAN = "\033[36m"
     WHITE = "\033[37m"
 
-all_colors = [Colors.BLACK, Colors.BLUE, Colors.CYAN, Colors.GREEN, Colors.MAGENTA, Colors.RED, Colors.WHITE, Colors.YELLOW]
+    BRIGHT_BLACK = "\033[90m"
+    BRIGHT_RED = "\033[91m"
+    BRIGHT_GREEN = "\033[92m"
+    BRIGHT_YELLOW = "\033[93m"
+    BRIGHT_BLUE = "\033[94m"
+    BRIGHT_MAGENTA = "\033[95m"
+    BRIGHT_CYAN = "\033[96m"
+    BRIGHT_WHITE = "\033[97m"
+
+all_colors = [
+    Colors.BLACK, 
+    Colors.BLUE, 
+    Colors.CYAN, 
+    Colors.GREEN, 
+    Colors.MAGENTA, 
+    Colors.RED, 
+    Colors.WHITE, 
+    Colors.YELLOW,
+    Colors.BRIGHT_BLACK,
+    Colors.BRIGHT_RED,
+    Colors.BRIGHT_GREEN,
+    Colors.BRIGHT_YELLOW,
+    Colors.BRIGHT_BLUE,
+    Colors.BRIGHT_MAGENTA,
+    Colors.BRIGHT_CYAN,
+    Colors.BRIGHT_WHITE
+]
