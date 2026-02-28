a0d5dde33888e909614dfa7fc0178529c036f4c9
japaneseTemmie <japanesetemmie64@proton.me>
2026-02-28 21:55:08 +0100

    Update typing for cachehelpers

--- helpers/cachehelpers.py
+++ helpers/cachehelpers.py
@@ -1,16 +1,16 @@
 """ Cache helper functions for discord.py bot. """
 
-from typing import Any
+from typing import Any, Hashable
 
 # Caching tools
-def store_cache(content: Any, id: str | int, cache: dict) -> None:
+def store_cache(content: Any, id: Hashable, cache: dict) -> None:
     if content:
         cache[id] = content
     else:
         invalidate_cache(id, cache) # Don't cache empty content
 
-def get_cache(cache: dict, id: str | int) -> Any | None:
+def get_cache(cache: dict, id: Hashable) -> Any | None:
     return cache.get(id)
     
-def invalidate_cache(id: str | int, cache: dict) -> None:
+def invalidate_cache(id: Hashable, cache: dict) -> None:
     cache.pop(id, None)
