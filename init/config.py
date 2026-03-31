3df165649b703a63601b27a92341ddd3aad9ff59
japaneseTemmie <japanesetemmie64@proton.me>
2026-03-31 21:25:26 +0200

    Config handling improvements Make some config.py functions private Optimize case where config file does not exist, by returning the default data directly after writing the JSON object

--- init/config.py
+++ init/config.py
@@ -10,29 +10,29 @@ from os.path import join, exists
 from time import sleep
 
 # Config manipulation
-def add_to_config(data: dict[str, Any], defaults: dict[str, Any]) -> None:
+def _add_to_config(data: dict[str, Any], defaults: dict[str, Any]) -> None:
     """ Compare `defaults`' keys with `data`'s keys and if any are missing, add them accordingly. """
 
     for key, value in defaults.items():
         if key not in data:
             data[key] = value
         elif isinstance(value, dict):
-            add_to_config(data[key], defaults[key])
+            _add_to_config(data[key], defaults[key])
 
-def add_missing_settings(config: dict[str, Any]) -> None:
+def _add_missing_settings(config: dict[str, Any]) -> None:
     """ Check config and compare it to default settings, if any keys are missing, add them accordingly. """
 
     default = get_default_config_data()
 
     # Check if keys are missing
-    add_to_config(config, default)
+    _add_to_config(config, default)
 
 def check_config(config: dict[str, Any]) -> dict[str, Any] | None:
     """ Checks if config file has any missing keys. If so, adds them with default values. """
     
     orig_config = deepcopy(config)
 
-    add_missing_settings(config)
+    _add_missing_settings(config)
 
     if config != orig_config:
         return config
@@ -46,17 +46,19 @@ def ensure_config(path: str, default_data: dict[str, Any]) -> dict[str, Any] | N
     
     if not exists(path):
         log(f"Creating config file because config.json does not exist at {path}")
+       
         result = write_file_json(path, default_data)
-
         if result == False:
             return None
 
         log(f"Created config file at {path}")
+        separator()
+
+        return deepcopy(default_data) # Reading the JSON and going through the check routine again despite already knowing the data is slightly inefficient
 
     log(f"Config file found at {path}")
     
     content = read_file_json(path)
-
     if content is None:
         return None
 
@@ -65,6 +67,7 @@ def ensure_config(path: str, default_data: dict[str, Any]) -> dict[str, Any] | N
 
     log("Checking config file..")
     sleep(0.5)
+
     new_content = check_config(content)
 
     if new_content is not None:
